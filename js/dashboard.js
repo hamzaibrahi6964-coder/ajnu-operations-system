5014580e0a86708d3d9e4867d5bc5e7b895b4010
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-08-24 15:12:53 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,11 +2,18 @@ alert("Dashboard JavaScript is working!");
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    const mobileMenu = document.getElementById("mobileMenu");
-    const sidebar = document.getElementById("sidebar");
+    const mobileMenu =
+        document.getElementById("mobileMenu");
+
+    const sidebar =
+        document.getElementById("sidebar");
+
 
     mobileMenu.addEventListener("click", function () {
-        sidebar.classList.toggle("show");
+
+        sidebar.style.transform =
+            "translateX(0)";
+
     });
 
 });
